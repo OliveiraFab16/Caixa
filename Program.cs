a51199ee92eaffa8cf6ed96b4a0311b7e5939e60
oliveirafab16 <fabriciocarvalho932@gmail.com>
2020-09-11 23:09:17 -0300

    nome na caixa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+------------+!");
+              Console.WriteLine("[Fabricio]");
+            Console.WriteLine("+------------+");
+        }
+    }
+}
